6432e3d0f5e4ab4535d3f3b82a9c9cc85cc64ae8
btomecsko <brandon.tomecsko92@gmail.com>
2022-03-23 17:09:56 -0500

    commit tree 853a97042cc52cdeae3b9ebbe26a8dcf4a04dd77

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name){
+    const newArray = [...cats, name];
+    return newArray;
+}
+
+function prependCat(name){
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+function removeLastCat(name){
+    const newArray = [...cats];
+    newArray.pop(name);
+    return newArray;
+}
+
+function removeFirstCat(name){
+    const newArray = [...cats];
+    newArray.shift(name);
+    return newArray;
+}
